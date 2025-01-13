78e8d84ca41575244908e35d96564bca84ef9103
Doug Haber <dhaber@leshylabs.com>
2025-01-12 23:51:30 -0500

    Add missing lair.util.argparse - Custom ArgumentParser() and exceptions
    This is used so that the /comfy command can behave properly with
    custom behaviors, unlike the defaults of argparse automatically
    exiting or printing help itself.

---
+++ lair/util/argparse.py
@@ -0,0 +1,26 @@
+import argparse
+
+
+class ArgumentParserExitException(Exception):
+    """Custom Exception for argparse to throw on exit, instead of actually exiting"""
+    pass
+
+class ArgumentParserHelpException(Exception):
+    pass
+
+
+class ErrorRaisingArgumentParser(argparse.ArgumentParser):
+    """Custom ArgumentParser() that throws exceptions so that behaviors could be handled differently"""
+    def error(self, message):
+        """Throw ArgumentError() on errors"""
+        raise argparse.ArgumentError(None, message)
+
+    def exit(self, status=0, message=None):
+        """Instead of exiting, throw an exception with the error"""
+        if message:
+            self._print_message(message)
+        raise ArgumentParserExitException(None, None)
+
+    def print_help(self, file=None):
+        """Override print_help to raise an exception with the help message."""
+        raise ArgumentParserHelpException(self.format_help())
